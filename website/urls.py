2687855d67da072657452126f8d03a519376f522
Frankie Belfiore <160235269@student.rnngroup.ac.uk>
2024-09-23 15:51:41 +0100

    'updated'

--- website/urls.py
+++ website/urls.py
@@ -6,6 +6,10 @@ urlpatterns = [
     path('', views.home, name=""),
 
     path('register', views.register, name="register"),
+
+    path('my-login', views.my_login, name="my-login"),
+
+    path('user-logout',views.user_logout,)
 ]
 
 
--- website/views.py
+++ website/views.py
@@ -1,10 +1,40 @@
 from .forms import CreateUserForm, LoginForm
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import auth
+from django.contrib.auth import  authenticate
 
-
-# Create your views here.
 def home(request):
     return render(request, 'website/index.html')
+
+def register(request):
+    form = CreateUserForm()
+    if request.method =="POST":
+        form = CreateUserForm(request.POST)
+    if form.is_valid():
+        form.save()
+        return redirect('')
+    context = {'form': form}
+
+    return render(request,'website/register.html', context=context)
+
+def my_login(request):
+    form = LoginForm(request, data=request.POST)
+
+    if request.method == "POST":
+        form = LoginForm(request, data=request.POST)
+
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+        if user is not  None:
+            auth.login(request, user)
+            #return redirect ('')
+
+    context = {'login_form':form}
+    return render(request,'website/my-login.html', context=context)
 
 def register(request):
     form = CreateUserForm()
@@ -13,8 +43,11 @@ def register(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+            return redirect('my-login')
+        
+    context = {'form':form}
 
+def user_logout(request):
 
-    context = {'form': form}
-
-    return render(request,'website/register.html', context=context)
+    auth.logout(request)
+    return redirect("my-login")
